fb869f39ee07a9619a7800edecc5a2e52f96dfe3
hs-black <zhangzunzhe@gmail.com>
2023-12-12 02:10:20 +0800

    update composite_demo

--- composite_demo/tool_registry.py
+++ composite_demo/tool_registry.py
@@ -165,10 +165,10 @@ def Music_Recommender(
     
 
     # calculator: 单个工具调用示例 3
-    llm = OpenAI(temperature=0)  
-    tools = load_tools(["ddg-search"], llm=llm) 
-    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True) 
-    agent.run("南昌明天多少度? 这个数的0.23次方是多少？我需要中文的输出结果")
+    # llm = OpenAI(temperature=0)  
+    # tools = load_tools(["ddg-search"], llm=llm) 
+    # agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True) 
+    # agent.run("南昌明天多少度? 这个数的0.23次方是多少？我需要中文的输出结果")
     return result
 
 if __name__ == "__main__":
