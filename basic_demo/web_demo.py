271331071da263de2c16a0f89d0010f0dfc570b3
suzhi <suz22@mails.tsinghua.edu.cn>
2023-12-11 17:24:51 -0800

    删除了.ipynb_checkpoints，更新了.gitignore和.gitmodules

--- basic_demo/web_demo.py
+++ basic_demo/web_demo.py
@@ -3,10 +3,10 @@ import gradio as gr
 import mdtex2html
 from utils import load_model_on_gpus
 
-model_path = "/root/autodl-tmp/chatglm3-6b"
+model_path = "/THUDM/chatglm3-6b"
 
-tokenizer = AutoTokenizer.from_pretrained("/root/autodl-tmp/chatglm3-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("/root/autodl-tmp/chatglm3-6b", trust_remote_code=True).cuda()
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+model = AutoModel.from_pretrained(model_path, trust_remote_code=True).cuda()
 
 # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
 # from utils import load_model_on_gpus
