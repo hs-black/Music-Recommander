f6cdf0f9b43b47d5e9de1705dc4dc018945dd794
root <root@mails.com>
2024-01-17 16:21:19 +0800

    erase openai keys

--- composite_demo/config.py
+++ composite_demo/config.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["BING_SUBSCRIPTION_KEY"] = "d7a75d130c9a4e61b18b554c096cb289"
-os.environ["OPENAI_API_KEY"] = "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"
-os.environ["BING_SEARCH_URL"] = "https://api.bing.microsoft.com/v7.0/search"
+# os.environ["BING_SUBSCRIPTION_KEY"] = "d7a75d130c9a4e61b18b554c096cb289"
+# os.environ["OPENAI_API_KEY"] = "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"
+# os.environ["BING_SEARCH_URL"] = "https://api.bing.microsoft.com/v7.0/search"
